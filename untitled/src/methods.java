91cbe1f2a0b4b70c56021cf5a50bfb2579244fb1
Anugrah-dev <anugrahdevansh2006@gmail.com>
2026-02-02 21:17:57 +0530

    Methods

---
+++ untitled/src/methods.java
@@ -0,0 +1,13 @@
+
+
+public class methods {
+    public static void main(String[] args) {
+        // method is a usable block of code that can be called when needed.
+
+        private static void sayHello( String name ) {
+            System.out.println("Hello " + name);
+        }
+
+        sayHello("John");
+    }
+}
