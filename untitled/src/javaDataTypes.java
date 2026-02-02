1417b80f2a4c47a0f11b26d5ef561181cf3c7f53
Anugrah-dev <anugrahdevansh2006@gmail.com>
2026-02-02 07:55:40 +0530

    Java Data Types

---
+++ untitled/src/javaDataTypes.java
@@ -0,0 +1,25 @@
+public class javaDataTypes {
+    public static void main(String[] args) {
+
+        // Primitive Data Types
+
+        String name = "JAVA";
+        char a = 'A';
+        boolean b = true;
+        int number = 111;
+
+        byte b1 = 128 ; // will give an error as byte range is -128 to 127
+
+        // byte and short are not used frequently, but it can be used when there is a shortage of memory or you know the exact value that will not be exceeded
+
+        // non-primitive data types
+        // strings, arrays, classes, interface
+        // string is a non-primitive data type as it is similar to array of characters and it has methods to perform certain operations
+
+        int[] marks = {1,2,3,4};
+        //array is accessed thru index starting from 0
+        System.out.println(marks[0]); // will print 1
+        System.out.println(marks[1]); // will print 2
+        System.out.println(marks[2]); // will print 3
+    }
+}
